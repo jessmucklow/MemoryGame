37202129909ce772d865ceaa98686e16bc5ff006
jess <jessicamucklow@gmail.com>
2022-07-03 23:16:30 -0700

    update7/3

--- js/main.js
+++ js/main.js
@@ -1,5 +1,15 @@
-/*----- card colors -------*/
-
+/*----- constants -------*/
+var SOURCE_CARDS = [
+    {img: 'https://upload.wikimedia.org/wikipedia/commons/thumb/b/b4/Color_icon_purple_v2.svg/800px-Color_icon_purple_v2.svg.png', matched: false},
+    {img: 'https://upload.wikimedia.org/wikipedia/commons/a/a7/MagentaIcon.png', matched: false},
+    {img: 'https://upload.wikimedia.org/wikipedia/commons/f/f1/Shades_of_light_blue.png', matched: false},
+    {img: 'hhttps://i0.wp.com/www.hisour.com/wp-content/uploads/2018/03/Shades-of-orange.jpg?fit=720%2C720&ssl=1&w=640', matched: false},
+    {img: 'https://upload.wikimedia.org/wikipedia/commons/thumb/7/7d/Color_icon_green.svg/800px-Color_icon_green.svg.png', matched: false},
+    {img: 'https://upload.wikimedia.org/wikipedia/commons/thumb/9/9e/Color_icon_yellow.svg/2048px-Color_icon_yellow.svg.png', matched: false},
+    {img: 'https://benambridge.files.wordpress.com/2014/12/red.png', matched: false},
+    {img: 'https://www.jenieyolland.com/wp-content/uploads/2021/10/1200px-Color_icon_cyan.svg_.png', matched: false}
+  ];
+  const CARD_BACK = 'https://upload.wikimedia.org/wikipedia/commons/thumb/1/1a/Color_icon_black.svg/800px-Color_icon_black.svg.png'; 
 
 
 /*----- app's active state (variables) -----*/
