e654e53735382c8c70c2b67f41f798c9f44ff0c4
jess <jessicamucklow@gmail.com>
2022-07-04 21:17:48 -0700

    7/4 pm update

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@ var SOURCE_CARDS = [
     {img: 'https://upload.wikimedia.org/wikipedia/commons/thumb/b/b4/Color_icon_purple_v2.svg/800px-Color_icon_purple_v2.svg.png', matched: false},
     {img: 'https://upload.wikimedia.org/wikipedia/commons/a/a7/MagentaIcon.png', matched: false},
     {img: 'https://upload.wikimedia.org/wikipedia/commons/f/f1/Shades_of_light_blue.png', matched: false},
-    {img: 'hhttps://i0.wp.com/www.hisour.com/wp-content/uploads/2018/03/Shades-of-orange.jpg?fit=720%2C720&ssl=1&w=640', matched: false},
+    {img: 'https://i0.wp.com/www.hisour.com/wp-content/uploads/2018/03/Shades-of-orange.jpg?fit=720%2C720&ssl=1&w=640', matched: false},
     {img: 'https://upload.wikimedia.org/wikipedia/commons/thumb/7/7d/Color_icon_green.svg/800px-Color_icon_green.svg.png', matched: false},
     {img: 'https://upload.wikimedia.org/wikipedia/commons/thumb/9/9e/Color_icon_yellow.svg/2048px-Color_icon_yellow.svg.png', matched: false},
     {img: 'https://benambridge.files.wordpress.com/2014/12/red.png', matched: false},
@@ -15,6 +15,7 @@ var SOURCE_CARDS = [
 /*----- app's active state (variables) -----*/
 let cards; //array of 16 cards in game board
 let firstCard; //first card clicked (card object) or null
+let ignoreClicks;
 
 
 /*----- cached elements -----*/
@@ -22,22 +23,24 @@ let firstCard; //first card clicked (card object) or null
 
 
 /*----- event listeners -----*/
-
+document.querySelector('container').addEventListener('click', handleChoice);
 
 
 /*----- functions -----*/
-Init();
+init();
 
 function init(){
   cards = getShuffledCards();
   firstCard = null;
+  ignoreClicks = false;
   render();
 }
 
 function render() {
   cards.forEach(function(card, idx) {
     const imgEl = document.getElementById(idx);
-    imgEl.src = card.img;
+    const src = (card.matched || card === firstCard) ? card.img : CARD_BACK;
+    imgEl.src = src;
     
   });
 }
@@ -46,7 +49,7 @@ function getShuffledCards(){
 let tempCards = [];
 let cards = [];
 for (let card of SOURCE_CARDS) {
-  tempCards.push(card, card);
+  tempCards.push({...card}, {...card});
 }
 while (tempCards.length) {
   let rndIdx = Math.floor(Math.random() * tempCards.length);
@@ -54,9 +57,31 @@ while (tempCards.length) {
   cards.push(card);
 
 }
-
-
 return cards;
 }
 
+//update all impacted state and then call render
+function handleChoice(evt) {
+const cardIdx = parseInt(evt.target.id);
+if (isNaN(cardIdx) || ignoreClicks) return;
+const card = cards[cardIdx]
+if (firstCard) {
+  if (firstCard.img === card.img) {
+    firstCard.matched = card.matched = true;
+    firstCard = null;
+    //correct match 
+  } else {
+    
+    // firstCard.matched = card.matched = false;
+    //incorrect match
+
+  }
+} else {
+    firstCard = card;
+}
+render();
+}
+
+
+// console.log(card)
 // change
